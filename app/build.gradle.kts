c65d6a9caf3d3a3da10509f31d82948309535627
veroanggraa <anggrainiveronica227@gmail.com>
2025-03-10 15:06:12 +0700

    set 35 sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.veroanggra.pennymate"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.veroanggra.pennymate"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
