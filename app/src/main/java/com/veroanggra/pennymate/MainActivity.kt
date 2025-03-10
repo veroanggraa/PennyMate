48a9bc644d18e3c6d9a6845a5916ae9f8fc70f0f
veroanggraa <anggrainiveronica227@gmail.com>
2025-03-10 15:06:59 +0700

    set main

--- app/src/main/java/com/veroanggra/pennymate/MainActivity.kt
+++ app/src/main/java/com/veroanggra/pennymate/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.veroanggra.pennymate.auth.AuthMainScreen
 import com.veroanggra.pennymate.ui.theme.PennyMateTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PennyMateTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    AuthMainScreen(modifier = Modifier)
                 }
             }
         }
