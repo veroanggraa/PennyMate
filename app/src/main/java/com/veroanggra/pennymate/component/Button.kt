ebc1b68335d6b64ed0e0fdcc523a283cf34d1620
veroanggraa <anggrainiveronica227@gmail.com>
2025-03-10 15:06:33 +0700

    add button custom

---
+++ app/src/main/java/com/veroanggra/pennymate/component/Button.kt
@@ -0,0 +1,112 @@
+package com.veroanggra.pennymate.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.veroanggra.pennymate.R
+import com.veroanggra.pennymate.ui.theme.BlueDark
+
+@Composable
+fun RectButtonTextFilled(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    label: String,
+    colorButton: Color,
+    colorLabel: Color,
+    width: Int = 280,
+    height: Int = 48
+) {
+    TextButton(
+        onClick = onClick,
+        modifier
+            .clip(RoundedCornerShape(4.dp))
+            .width(width.dp)
+            .height(height.dp)
+            .background(color = colorButton)
+    ) {
+        Text(
+            text = label, style = TextStyle(
+                fontSize = 15.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = colorLabel
+            )
+        )
+    }
+}
+
+@Composable
+fun RectButtonTextIconOutline(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    label: String,
+    colorButton: Color,
+    colorOutline: Color,
+    colorLabel: Color,
+    width: Int = 280,
+    height: Int = 48,
+    icon: Int = R.drawable.ic_google
+) {
+    Row(
+        modifier
+            .clip(RoundedCornerShape(4.dp))
+            .border(width = 1.dp, color = colorOutline, shape = RoundedCornerShape(4.dp))
+            .width(width.dp)
+            .height(height.dp)
+            .background(color = colorButton)
+            .clickable { onClick },
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(icon),
+            contentDescription = null,
+            modifier = modifier.padding(start = 16.dp)
+                .size(24.dp)
+        )
+        Text(
+            text = label, style = TextStyle(
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Medium,
+                color = colorLabel
+            ),
+            modifier = modifier.weight(1f), textAlign = TextAlign.Center
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun RectButtonTextFilledPreview() {
+    RectButtonTextFilled(
+        modifier = Modifier,
+        onClick = {},
+        label = "Sign In",
+        colorButton = BlueDark,
+        colorLabel = Color.White
+    )
+}
