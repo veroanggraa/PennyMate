951768b4426d0fb4b1e0da9e538d694ffd216e5f
veroanggraa <anggrainiveronica227@gmail.com>
2025-03-26 10:44:37 +0700

    add outline input field

--- app/src/main/java/com/veroanggra/pennymate/component/TextField.kt
+++ app/src/main/java/com/veroanggra/pennymate/component/TextField.kt
@@ -1,13 +1,20 @@
 package com.veroanggra.pennymate.component
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.veroanggra.pennymate.ui.theme.GrayB6B7B8
 
@@ -16,17 +23,21 @@ fun CustomTextField(
     modifier: Modifier = Modifier,
     value: String,
     onValueChange: (String) -> Unit,
-    // icons param
     leadingIcon: Painter? = null,
     onTrailingIconClick: () -> Unit = {},
     trailingIcon: Painter? = null,
+    hint: String,
+    hintColor: Color,
+    padding: Dp
 ) {
     OutlinedTextField(
-        modifier = modifier,
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(start = padding, end = padding),
         colors = OutlinedTextFieldDefaults.colors(GrayB6B7B8),
         value = value,
         onValueChange = onValueChange,
-        // icons
         leadingIcon = {
             if (leadingIcon != null)
                 Icon(
@@ -42,6 +53,9 @@ fun CustomTextField(
                         painter = trailingIcon, contentDescription = null
                     )
                 }
+        },
+        placeholder = {
+            Text(text = hint, color = hintColor)
         }
     )
 }
