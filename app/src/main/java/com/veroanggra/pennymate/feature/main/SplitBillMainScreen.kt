b2acfed1c3a13607a1c3fcc51675539309a605d3
veroanggraa <anggrainiveronica227@gmail.com>
2025-03-28 23:02:49 +0700

    add column

--- app/src/main/java/com/veroanggra/pennymate/feature/main/SplitBillMainScreen.kt
+++ app/src/main/java/com/veroanggra/pennymate/feature/main/SplitBillMainScreen.kt
@@ -2,11 +2,18 @@ package com.veroanggra.pennymate.feature.main
 
 import android.Manifest
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberPermissionState
 import com.google.accompanist.permissions.PermissionRequired
+import com.veroanggra.pennymate.R
 
 
 @OptIn(ExperimentalPermissionsApi::class)
@@ -16,7 +23,14 @@ fun SplitBillMainScreen(modifier: Modifier = Modifier) {
     PermissionRequired(
         permissionState = cameraPermissionState,
         permissionNotGrantedContent = {
-            Column { }
+            Column(
+                modifier = modifier
+                    .fillMaxSize()
+                    .padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(stringResource(R.string.app_name))
+
+            }
         },
         permissionNotAvailableContent = {
             Column { }
