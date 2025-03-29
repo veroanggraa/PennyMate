6698a4ccf6a55ea7056457ae8dfae9348102ef48
veroanggraa <anggrainiveronica227@gmail.com>
2025-03-29 11:43:52 +0700

    add navigation setuo

--- app/src/main/java/com/veroanggra/pennymate/NavigationSetup.kt
+++ app/src/main/java/com/veroanggra/pennymate/NavigationSetup.kt
@@ -9,6 +9,7 @@ import com.veroanggra.pennymate.auth.main.AuthScreen
 import com.veroanggra.pennymate.auth.signin.SignInScreen
 import com.veroanggra.pennymate.auth.signup.SignUpScreen
 import com.veroanggra.pennymate.feature.landing.SplitBillLandingScreen
+import com.veroanggra.pennymate.feature.main.SplitBillMainScreen
 
 sealed class Screen(val route: String) {
     data object MainAuth : Screen("mainauth")
@@ -16,6 +17,7 @@ sealed class Screen(val route: String) {
     data object Home : Screen("home")
     data object SignIn : Screen("signin")
     data object SplitBillLandingScreen : Screen("split_bill_landing")
+    data object SplitBillMainScreen : Screen("split_bill_main")
 }
 
 @Composable
@@ -26,5 +28,7 @@ fun MainNavigation(modifier: Modifier = Modifier) {
         composable(Screen.SignUp.route) { SignUpScreen(modifier, navController) }
         composable(Screen.SignIn.route) { SignInScreen(modifier, navController) }
         composable(Screen.SplitBillLandingScreen.route) { SplitBillLandingScreen(modifier, navController) }
+        composable(Screen.SplitBillMainScreen.route) { SplitBillMainScreen(modifier) }
+
     }
 }
