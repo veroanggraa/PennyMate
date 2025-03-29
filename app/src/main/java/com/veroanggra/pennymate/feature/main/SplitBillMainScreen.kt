527ab68076414844933a79afe6a534630c51b7e1
veroanggraa <anggrainiveronica227@gmail.com>
2025-03-29 11:43:26 +0700

    split bill permission

--- app/src/main/java/com/veroanggra/pennymate/feature/main/SplitBillMainScreen.kt
+++ app/src/main/java/com/veroanggra/pennymate/feature/main/SplitBillMainScreen.kt
@@ -1,9 +1,14 @@
 package com.veroanggra.pennymate.feature.main
 
 import android.Manifest
+import android.widget.Space
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -26,14 +31,37 @@ fun SplitBillMainScreen(modifier: Modifier = Modifier) {
             Column(
                 modifier = modifier
                     .fillMaxSize()
-                    .padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(stringResource(R.string.app_name))
+                Text(text = stringResource(R.string.txt_camera_permission_info_first))
+                Spacer(modifier = modifier.height(8.dp))
+                Row {
+                    Button(onClick = {
+                        cameraPermissionState.launchPermissionRequest()
+                    }) {
+                        Text(text = "Grant Permission")
+                    }
+                }
 
             }
         },
         permissionNotAvailableContent = {
-            Column { }
+            Column(
+                modifier = modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = stringResource(R.string.txt_camera_permission_info_second))
+                Spacer(modifier = modifier.height(8.dp))
+                Row {
+                    Button(onClick = {
+                    }) {
+                        Text(text = "Open Settings")
+                    }
+                }
+            }
         }
     ) {}
 }
