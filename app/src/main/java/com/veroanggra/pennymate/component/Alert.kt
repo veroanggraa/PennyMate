d05154f6f1dd4c4f38baae6c5c51ea64fde60ecf
veroanggraa <anggrainiveronica227@gmail.com>
2025-04-04 23:15:01 +0700

    add alert parameter

--- app/src/main/java/com/veroanggra/pennymate/component/Alert.kt
+++ app/src/main/java/com/veroanggra/pennymate/component/Alert.kt
@@ -2,18 +2,19 @@ package com.veroanggra.pennymate.component
 
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 
 @Composable
-fun CustomAlert(modifier: Modifier = Modifier, onDismissRequest: () -> Unit) {
+fun CustomAlert(modifier: Modifier = Modifier, title: String ,onDismissRequest: () -> Unit) {
+    val openDialog = remember { mutableStateOf(false)  }
     AlertDialog(
-        onDismissRequest = {
-        },
-        title = {
-        },
-        text = {
-        },
+        onDismissRequest = {},
+        title = {},
+        text = {},
         confirmButton = {
             Button(
 
