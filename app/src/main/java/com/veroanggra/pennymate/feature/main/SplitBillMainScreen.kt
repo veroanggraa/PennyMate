eed186470daebc16f51ebacd833f6810f57532da
veroanggraa <anggrainiveronica227@gmail.com>
2025-06-10 16:01:40 +0700

    add main screen for split bill

--- app/src/main/java/com/veroanggra/pennymate/feature/main/SplitBillMainScreen.kt
+++ app/src/main/java/com/veroanggra/pennymate/feature/main/SplitBillMainScreen.kt
@@ -1,68 +1,138 @@
 package com.veroanggra.pennymate.feature.main
 
 import android.Manifest
-import android.widget.Space
+import android.util.Log
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ExperimentalGetImage
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberPermissionState
-import com.google.accompanist.permissions.PermissionRequired
-import com.veroanggra.pennymate.R
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.TextRecognizer
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+import com.veroanggra.pennymate.component.CameraPermissionDialog
+import java.lang.Exception
+import java.util.concurrent.Executors
 
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun SplitBillMainScreen(modifier: Modifier = Modifier) {
     val cameraPermissionState = rememberPermissionState(Manifest.permission.CAMERA)
-    PermissionRequired(
-        permissionState = cameraPermissionState,
-        permissionNotGrantedContent = {
-            Column(
-                modifier = modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(text = stringResource(R.string.txt_camera_permission_info_first))
-                Spacer(modifier = modifier.height(8.dp))
-                Row {
-                    Button(onClick = {
-                        cameraPermissionState.launchPermissionRequest()
-                    }) {
-                        Text(text = "Grant Permission")
-                    }
-                }
+    var showCameraPermissionDialog by remember { mutableStateOf(false) }
+    var scannerBillText by remember { mutableStateOf("") }
 
-            }
-        },
-        permissionNotAvailableContent = {
-            Column(
-                modifier = modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(text = stringResource(R.string.txt_camera_permission_info_second))
-                Spacer(modifier = modifier.height(8.dp))
-                Row {
-                    Button(onClick = {
-                    }) {
-                        Text(text = "Open Settings")
-                    }
+    LaunchedEffect(cameraPermissionState.hasPermission) {
+        if (!cameraPermissionState.hasPermission) {
+            showCameraPermissionDialog = true
+        } else {
+            showCameraPermissionDialog = false
+        }
+    }
+
+    if (showCameraPermissionDialog) {
+        CameraPermissionDialog(
+            onDismissRequest = { showCameraPermissionDialog = false },
+            cameraPermissionState = cameraPermissionState,
+            modifier = Modifier
+        )
+    } else {
+        if (cameraPermissionState.hasPermission) {
+            CameraScanScreen(modifier = modifier, onBillScanned = { text ->
+                scannerBillText = text
+                Log.d("SplitBillMainScreen", "Scanned Bill Text: $scannerBillText")
+            })
+        } else {
+            Column {
+                Text(text = "Camera permission denied, cannot scan bill")
+                Button(onClick = { showCameraPermissionDialog = true }) {
+                    Text(text = "Grant Permission")
                 }
             }
         }
-    ) {}
+    }
 }
 
+@Composable
+fun CameraScanScreen(modifier: Modifier = Modifier, onBillScanned: (String) -> Unit) {
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
+    val textRecognizer =
+        remember { TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS) }
+    Box(modifier = modifier.fillMaxSize()) {
+        AndroidView(
+            modifier = Modifier.fillMaxSize(),
+            factory = { ctx ->
+                val previewView = PreviewView(ctx)
+                val cameraProvider = cameraProviderFuture.get()
+                val preview = androidx.camera.core.Preview.Builder().build().also {
+                    it.setSurfaceProvider(previewView.surfaceProvider)
+                }
+                val imageAnalyzer = ImageAnalysis.Builder()
+                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST).build().also {
+                        it.setAnalyzer(Executors.newSingleThreadExecutor()) { imageProxy ->
+                            processImageProxy(textRecognizer, imageProxy, onBillScanned)
+                        }
+                    }
+                val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+                try {
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(
+                        lifecycleOwner, cameraSelector, preview, imageAnalyzer
+                    )
+                } catch (e: Exception) {
+                    Log.e("CameraX", "Use case binding failed, e")
+                }
+                previewView
+            }
+        )
+    }
+}
+
+@androidx.annotation.OptIn(ExperimentalGetImage::class)
+private fun processImageProxy(
+    textRecognizer: TextRecognizer,
+    imageProxy: ImageProxy,
+    onBillScanned: (String) -> Unit
+) {
+    imageProxy.image?.let { mediaImage ->
+        val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+        textRecognizer.process(image)
+            .addOnSuccessListener { visionText ->
+                val detectedText = visionText.text
+                if (detectedText.isNotBlank()) {
+                    Log.d("Bill Scanner", "Detected text : $detectedText")
+                    onBillScanned(detectedText)
+                } else {
+                    Log.d("Bill Scanner", "No meaningful text detected in this frame.")
+
+                }
+                imageProxy.close()
+            }
+            .addOnFailureListener { e ->
+                Log.e("Bill Scanner", "Text recognation", e)
+                imageProxy.close()
+            }
+    } ?: run { imageProxy.close() }
+}
