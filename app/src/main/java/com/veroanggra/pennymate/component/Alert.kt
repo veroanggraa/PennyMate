7392ab8b90572080baeefa0ab8a8510de59ca15d
veroanggraa <anggrainiveronica227@gmail.com>
2025-06-10 16:01:03 +0700

    make custom alert

--- app/src/main/java/com/veroanggra/pennymate/component/Alert.kt
+++ app/src/main/java/com/veroanggra/pennymate/component/Alert.kt
@@ -1,16 +1,31 @@
 package com.veroanggra.pennymate.component
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionState
 
 @Composable
-fun CustomAlert(modifier: Modifier = Modifier, title: String, content : String, onDismissRequest: () -> Unit) {
-    val openDialog = remember { mutableStateOf(false)  }
+fun CustomAlert(
+    modifier: Modifier = Modifier,
+    title: String,
+    content: String,
+    onDismissRequest: () -> Unit
+) {
+    val openDialog = remember { mutableStateOf(false) }
     AlertDialog(
         onDismissRequest = {},
         title = { Text(title) },
@@ -26,7 +41,6 @@ fun CustomAlert(modifier: Modifier = Modifier, title: String, content : String, 
         },
         dismissButton = {
             Button(
-
                 onClick = {
                     openDialog.value = false
                 }) {
@@ -34,5 +48,43 @@ fun CustomAlert(modifier: Modifier = Modifier, title: String, content : String, 
             }
         }
     )
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun CameraPermissionDialog(
+    modifier: Modifier = Modifier,
+    onDismissRequest: () -> Unit,
+    cameraPermissionState: PermissionState
+) {
+        AlertDialog(
+            onDismissRequest = {
+                onDismissRequest()
+            },
+            title = { Text(text = "Camera Permission Required") },
+            text = {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = stringResource(com.veroanggra.pennymate.R.string.txt_camera_permission_info_first))
+                    Spacer(modifier = modifier.height(8.dp))
+                }
+            },
+            confirmButton = {
+                Button(onClick = {
+                    cameraPermissionState.launchPermissionRequest()
+                    onDismissRequest()
+                }) {
+                    Text(text = "Grant Permission")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = {
+                    onDismissRequest()
+                }) {
+                    Text(text = "Not Now")
+                }
+            }
+        )
 
 }
